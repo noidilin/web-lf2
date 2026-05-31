d831ee64a6dde866c3136b8914a66118f96252d9
noidilin <linganinja.0120@gmail.com>
2026-05-31 12:36:55 +0800

    fix(ci): use github environment var as audit gate for deployment

--- infra/catalog/modules/deployment-identity/main.tf
+++ infra/catalog/modules/deployment-identity/main.tf
@@ -59,7 +59,7 @@ resource "aws_iam_role" "github_apply" {
             "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
           }
           StringLike = {
-            "token.actions.githubusercontent.com:sub" = "repo:${var.github_repo}:ref:refs/heads/main"
+            "token.actions.githubusercontent.com:sub" = "repo:${var.github_repo}:environment:${var.environment}"
           }
         }
       }
